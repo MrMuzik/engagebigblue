06e73628ca189dea79862217c7fe3804fbad1a9a
Bryan Markham <muzik702@gmail.com>
2024-07-04 10:20:37 -0400

    testing routes 3

---
+++ .netlify/server/nodes/4.js
@@ -0,0 +1,8 @@
+
+
+export const index = 4;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/success/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/4.-XPJhcc4.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.BjOwGYW9.js"];
+export const stylesheets = [];
+export const fonts = [];
