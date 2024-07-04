9bb577212fa7348fb799e7f3476a75b0452a49a6
Bryan Markham <muzik702@gmail.com>
2024-07-04 08:23:14 -0400

    debuging forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,4 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<form name="contact" method="POST" data-netlify="true" action="/" onsubmit="submitForm(event)">
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: 
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-  <script>
+<script>
     async function submitForm(event) {
       event.preventDefault();
       const form = event.target;
@@ -55,5 +29,30 @@
         alert('An error occurred during form submission.');
       }
     }
-  </script>
-  
+</script>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form name="contact" method="POST" data-netlify="true" action="/" onsubmit="submitForm(event)">
+    <input type="hidden" name="form-name" value="contact" />
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Your Role: 
+        <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select>
+      </label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
