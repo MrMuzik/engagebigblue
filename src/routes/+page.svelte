d3d4a35582db7f3c1e5a9794e0701674dcb043c6
Bryan Markham <muzik702@gmail.com>
2024-07-04 09:06:02 -0400

    debuging forms new adapter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,13 @@
-<script>
-    async function submitForm(event) {
-      event.preventDefault();
-      const form = event.target;
-  
-      const data = new FormData(form);
-      const object = {};
-      data.forEach((value, key) => {
-        object[key] = value;
-      });
-      const json = JSON.stringify(object);
-  
-      try {
-        const response = await fetch(form.action, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: json
-        });
-  
-        if (response.ok) {
-          alert('Form submitted successfully!');
-        } else {
-          alert('Form submission failed.');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-        alert('An error occurred during form submission.');
-      }
-    }
-</script>
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<form name="contact" method="POST" data-netlify="true" action="/" onsubmit="submitForm(event)">
-    <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: 
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <input type="hidden" name="form-name" value="netlify-form-example" />
+    <label for="name">Name</label>
+    <input name="name" id="name" required placeholder="Name" type="text" />
+    <label for="email">Email</label>
+    <input name="email" id="email" required placeholder="Email" type="email" />
+    <label for="message">Message</label>
+    <input name="message" id="message" required placeholder="Message" type="text" />
+    <input type="submit" value="Submit" />
+</form>
